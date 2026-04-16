d2aad63860dd42c0a5ad60ace5ce4e2b0f83c87c
Supreeth-Sharma <supreethsharmayv01@gmail.com>
2026-04-16 12:06:31 +0530

    Phase4: read HEAD to determine parent commit

--- commit.c
+++ commit.c
@@ -204,3 +204,12 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     return -1;
 }
+
+ObjectID parent;
+
+if (head_read(&parent) == 0) {
+    commit.parent = parent;
+    commit.has_parent = 1;
+} else {
+    commit.has_parent = 0;
+}
